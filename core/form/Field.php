644823936c0ad51238bf42d254ea2dbd24134c3f
Yao Jean-Claude <jeandev84@yahoo.com>
2021-06-02 04:33:56 +0300

    form

--- core/form/Field.php
+++ core/form/Field.php
@@ -11,6 +11,15 @@ use app\core\Model;
 class Field
 {
 
+     public const TYPE_TEXT = 'text';
+     public const TYPE_PASSWORD = 'password';
+     public const TYPE_NUMBER = 'number';
+     public const TYPE_EMAIL = 'email';
+
+
+
+
+     public string $type;
      public Model $model;
      public string $attribute;
 
@@ -23,6 +32,7 @@ class Field
      */
      public function __construct(Model $model, $attribute)
      {
+         $this->type = self::TYPE_TEXT;
          $this->model = $model;
          $this->attribute = $attribute;
      }
@@ -30,15 +40,29 @@ class Field
 
      public function __toString()
      {
-          return sprintf('<div class="col">
+          return sprintf('
             <div class="form-group">
                 <label>%s</label>
-                <input type="text" name="%s"  value="%s" class="form-control%s">
-            </div>
-        </div>',
+                <input type="%s" name="%s"  value="%s" class="form-control%s">
+                <div class="invalid-feedback">
+                    %s
+                </div>
+            </div>',
+$this->attribute,
+        $this->type,
         $this->attribute,
-        $this->attribute,
-        $this->model->{$this->attribute}
-          );
+        $this->model->{$this->attribute},
+        /*$this->model->hasError($this->attribute) ? ' is-invalid' : ' is-valid', */
+        $this->model->hasError($this->attribute) ? ' is-invalid' : ' ',
+        $this->model->getFirstError($this->attribute)
+      );
+     }
+
+
+
+     public function passwordField()
+     {
+         $this->type = self::TYPE_PASSWORD;
+         return $this;
      }
 }
