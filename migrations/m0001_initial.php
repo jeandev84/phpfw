c9891d0814d6c37e9b26e54c2671dd41a4d5db9e
Yao Jean-Claude <jeandev84@yahoo.com>
2021-06-02 07:14:45 +0300

    Db Model and refactoring code

--- migrations/m0001_initial.php
+++ migrations/m0001_initial.php
@@ -5,12 +5,25 @@ class m0001_initial {
 
       public function up()
       {
-         echo "Applying migration". PHP_EOL;
+          $db = \app\core\Application::$app->db;
+
+          $SQl = "CREATE TABLE IF NOT EXISTS users (
+                     id int AUTO_INCREMENT PRIMARY KEY,
+                     email VARCHAR(255) NOT NULL,
+                     firstname VARCHAR(255) NOT NULL,
+                     lastname VARCHAR(255) NOT NULL,
+                     status TINYINT DEFAULT 0 NOT NULL,
+                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+                  ) ENGINE=INNODB;";
+
+          $db->pdo->exec($SQl);
       }
 
 
       public function down()
       {
-          echo "Down migration". PHP_EOL;
+          $db = \app\core\Application::$app->db;
+          $SQl = "DROP TABLE users;";
+          $db->pdo->exec($SQl);
       }
 }
--- models/User.php
+++ models/User.php
@@ -2,27 +2,44 @@
 namespace app\models;
 
 
-use app\core\Model;
+use app\core\DbModel;
+
 
 
 /**
- * Class RegisterModel
+ * Class User
  * @package app\models
 */
-class RegisterModel extends Model
+class User extends DbModel
 {
+    const STATUS_INACTIVE = 0;
+    const STATUS_ACTIVE   = 1;
+    const STATUS_DELETED  = 2;
+
+
+
     public string $firstname = '';
     public string $lastname = '';
     public string $email = '';
+    public int $status = self::STATUS_INACTIVE;
     public string $password = '';
     public string $confirmPassword = '';
 
 
-    public function register()
+    /**
+     * Create new user
+    */
+    public function save()
     {
-        echo "Creating new user";
+        $this->status = self::STATUS_INACTIVE;
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
+        return parent::save();
     }
 
+
+    /**
+     * @return array[]
+    */
     public function rules(): array
     {
         return [
@@ -32,5 +49,21 @@ class RegisterModel extends Model
             'password'  => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 24]],
             'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
         ];
+    }
+
+
+    /**
+     * @return string
+    */
+    public function tableName(): string
+    {
+         return 'users';
+    }
+
+
+
+    public function attributes(): array
+    {
+        return ['firstname', 'lastname', 'email', 'password'];
     }
 }
